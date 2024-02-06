7e5a905f23a26ccaac9a70fcac4b5a0fdf6b06c3
Mohammad Tafreshi <mn.tafreshi@gmail.com>
2024-02-06 21:10:11 +0330

    sss

--- src/main/java/com/example/demo/CorsConfig.java
+++ src/main/java/com/example/demo/CorsConfig.java
@@ -26,25 +26,20 @@ import java.util.Arrays;
             System.out.println("doFilter ........");
             CorsConfiguration corsConfiguration = new CorsConfiguration();
 
+
+
             HttpServletResponse response = (HttpServletResponse) res;
             HttpServletRequest request = (HttpServletRequest) req;
-            response.setHeader("Access-Control-Allow-Origin", "*");
+
+            String origin = request.getHeader("Origin");
+            System.out.println("origin is "+origin);
+            response.setHeader("Access-Control-Allow-Origin", origin );
             response.setHeader("Access-Control-Allow-Credentials", "true");
             response.setHeader("Access-Control-Allow-Methods",
                     "ACL, CANCELUPLOAD, CHECKIN, CHECKOUT, COPY, DELETE, GET, HEAD, LOCK, MKCALENDAR, MKCOL, MOVE, OPTIONS, POST, PROPFIND, PROPPATCH, PUT, REPORT, SEARCH, UNCHECKOUT, UNLOCK, UPDATE, VERSION-CONTROL");
             response.setHeader("Access-Control-Max-Age", "3600");
             response.setHeader("Access-Control-Allow-Headers",
                     "Origin, X-Requested-With, Content-Type, Accept, Key, Authorization");
-
-            corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
-            corsConfiguration.setAllowedMethods(Arrays.asList("GET","POST"));
-            corsConfiguration.setAllowedHeaders(Arrays.asList("content-type"));
-            corsConfiguration.setAllowCredentials(true);
-            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-            source.registerCorsConfiguration("/**", corsConfiguration);
-
-
-
 
             if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
                 response.setStatus(HttpServletResponse.SC_OK);
